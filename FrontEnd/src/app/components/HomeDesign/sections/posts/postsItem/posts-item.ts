7ee7ca173c64f467682b9363813b2da242239d68
Kristijan <kristijanstojanovic11@gmail.com>
2026-06-26 19:45:09 +0200

    Added navigation

--- FrontEnd/src/app/components/HomeDesign/sections/posts/postsItem/posts-item.ts
+++ FrontEnd/src/app/components/HomeDesign/sections/posts/postsItem/posts-item.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { PostsInterface } from '../../../../../interfaces/posts-interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-item',
-  imports: [],
+  imports: [RouterLink],
   template: `                  <article class="flex min-h-[340px] flex-col rounded-lg border border-slate-200 bg-[#f8fbff] p-6 shadow-sm">
                         <div class="mb-4 flex flex-wrap gap-2">
                             <span class="rounded-lg bg-indigo-50 px-3 py-1 text-xs font-black text-indigo-700">React</span>
@@ -16,7 +17,7 @@ import { PostsInterface } from '../../../../../interfaces/posts-interface';
                         <div class="mt-5 border-t border-slate-200 pt-4 text-sm">
                             <p class="font-bold text-slate-800">{{ post().phone }}</p>
                             <p class="mt-1 font-bold text-indigo-600">{{ post().email }}</p>
-                            <a href="post-detail.html" class="mt-4 inline-flex rounded-lg bg-slate-950 px-4 py-2 text-sm font-bold text-white">View post</a>
+                            <a routerLink="/posts/{{post().id}}" class="mt-4 inline-flex rounded-lg bg-slate-950 px-4 py-2 text-sm font-bold text-white">View post</a>
                         </div>  
                     </article> `,
   styles: ``,
